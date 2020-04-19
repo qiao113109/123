28ddc52731ac652b0f0b04e719c1cbefac6a76a8
今天你也要开心 <qiao113109@163.com>
2020-04-19 15:32:01 +0800

    博客主题文件

---
+++ Sakura-master/footer.php
@@ -0,0 +1,184 @@
+<?php
+/**
+ * The template for displaying the footer.
+ *
+ * Contains the closing of the #content div and all content after.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package Sakura
+ */
+
+?>
+	</div><!-- #content -->
+	<?php 
+		if(akina_option('general_disqus_plugin_support')){
+			get_template_part('layouts/duoshuo');
+		}else{
+			comments_template('', true); 
+		}
+	?>
+<script src="https://cdn.jsdelivr.net/gh/yremp/live2d@2.0/autoload.js"></script>
+<script src="https://cdn.jsdelivr.net/gh/fz6m/Private-web@1.4/js/custom/slide.js"></script>
+	</div><!-- #page Pjax container-->
+	<footer id="colophon" class="site-footer" role="contentinfo">
+		<div class="site-info" theme-info="Sakura v<?php echo SAKURA_VERSION; ?>">
+			<div class="footertext">
+				<div class="img-preload">
+					<img src="https://cdn.jsdelivr.net/gh/moezx/cdn@3.1.9/img/Sakura/images/wordpress-rotating-ball-o.svg">
+					<img src="https://cdn.jsdelivr.net/gh/moezx/cdn@3.1.9/img/Sakura/images/disqus-preloader.svg">
+				</div>
+				<p style="color: #666666;"><?php echo akina_option('footer_info', ''); ?></p>
+			</div>
+			<!-- footer -->
+			<div class="footer-device">
+		
+		<p style="font-family: 'Ubuntu', sans-serif;">
+				<span style="color: rgba(0,0,0,.4);font-size:16px">
+			<!-- 备案信息 -->
+					© 2020 Moon <a href="http://www.beian.miit.gov.cn/" target="_blank" style="color:  rgba(0,0,0,.4);;text-decoration:none">豫ICP备20009876号</a>
+			</p>
+			<!-- 底部图标 -->
+			<a href="https://www.aliyun.com/" target="_blank" alt="ALiYun" rel="nofollow">
+			<img src="https://yremp.live/wp-content/uploads/2019/07/aliyun.png" alt="aliyun" style="height: 1.7em; max-height: 1.8em;padding-bottom: 0px;">
+			</a>   
+
+			<a href="https://www.jsdelivr.com/" target="_blank" alt="Free CDN" rel="nofollow">
+			<img src="https://www.yremp.live/images/2019/08/17/JsDelivr_Logo0bd9a.png" alt="jsDelivr" style="height: 2em; max-height: 2em;padding-bottom: 0px;">
+			</a>  
+
+			<a href="https://github.com/" target="_blank" alt="Github" rel="nofollow">
+			<img src="https://yremp.live/wp-content/uploads/2019/07/Github.png" alt="github" style="height: 2.2em; max-height: 2.5em;padding-bottom: 0px;">
+			</a>  
+
+			<a href="https://2heng.xin" target="_blank" alt="Mashiro" rel="nofollow">
+			<img src="https://cdn.jsdelivr.net/gh/moezx/cdn@3.0.8/img/logo/mashiro-logo.png" alt="Mashiro" style="height: 2.5em; max-height: 2em;padding-bottom: 0px; margin-bottom:0.2em">
+			</a>
+		</div>
+
+
+		</div><!-- .site-info -->
+	</footer><!-- #colophon -->
+	<div class="openNav no-select">
+		<div class="iconflat no-select">	 
+			<div class="icon"></div>
+		</div>
+		<div class="site-branding">
+			<?php if (akina_option('akina_logo')){ ?>
+			<div class="site-title"><a href="<?php bloginfo('url');?>" ><img src="<?php echo akina_option('akina_logo'); ?>"></a></div>
+			<?php }else{ ?>
+			<!-- <h1 class="site-title"><a href="<?php bloginfo('url');?>" ><?php bloginfo('name');?></a></h1> -->
+			<?php } ?>
+		</div>
+	</div><!-- m-nav-bar -->
+	</section><!-- #section -->
+	<!-- m-nav-center -->
+	<div id="mo-nav">
+		<div class="m-avatar">
+			<?php $ava = akina_option('focus_logo') ? akina_option('focus_logo') : get_template_directory_uri().'/images/avatar.jpg'; ?>
+			<img src="<?php echo $ava ?>">
+		</div>
+		<div class="m-search">
+			<form class="m-search-form" method="get" action="<?php echo home_url(); ?>" role="search">
+				<input class="m-search-input" type="search" name="s" placeholder="<?php _e('Search...', 'sakura') /*搜索...*/?>" required>
+			</form>
+		</div>
+		<?php wp_nav_menu( array( 'depth' => 2, 'theme_location' => 'primary', 'container' => false ) ); ?>
+	</div><!-- m-nav-center end -->
+	<a class="cd-top faa-float animated "></a>
+	<button id="moblieGoTop" title="Go to top"><i class="fa fa-chevron-up" aria-hidden="true"></i></button>
+	<button id="moblieDarkLight"><i class="fa fa-moon-o" aria-hidden="true"></i></button>
+	<!-- search start -->
+	<form class="js-search search-form search-form--modal" method="get" action="<?php echo home_url(); ?>" role="search">
+		<div class="search-form__inner">
+		<?php if(akina_option('live_search')){ ?>
+			<div class="micro">
+				<i class="iconfont icon-search"></i>
+				<input id="search-input" class="text-input" type="search" name="s" placeholder="<?php _e('Want to find something?', 'sakura') /*想要找点什么呢*/?>" required>
+			</div>
+			<div class="ins-section-wrapper">
+                <a id="Ty" href="#"></a>
+                <div class="ins-section-container" id="PostlistBox"></div>
+            </div>
+		<?php }else{ ?>
+			<div class="micro">
+				<p class="micro mb-"><?php _e('Want to find something?', 'sakura') /*想要找点什么呢*/?></p>
+				<i class="iconfont icon-search"></i>
+				<input class="text-input" type="search" name="s" placeholder="<?php _e('Search', 'sakura') ?>" required>
+			</div>
+		<?php } ?>
+		</div>
+		<div class="search_close"></div>
+	</form>
+	<!-- search end -->
+<?php wp_footer(); ?>
+<?php if(akina_option('site_statistics')){ ?>
+<div class="site-statistics">
+<script type="text/javascript"><?php echo akina_option('site_statistics'); ?></script>
+</div>
+<?php } ?>
+<div class="changeSkin-gear no-select" style="bottom: -999px;">
+    <div class="keys">
+        <span id="open-skinMenu">
+		<i class="iconfont icon-gear inline-block rotating"></i>&nbsp; 切换主题 | SCHEME TOOL 
+        </span>
+    </div>
+</div>
+<div class="skin-menu no-select">
+    <div class="theme-controls row-container">
+        <ul class="menu-list">
+            <li id="white-bg">
+                <i class="fa fa-television" aria-hidden="true"></i>
+            </li><!--Default-->
+            <li id="sakura-bg">
+                <i class="iconfont icon-sakura"></i>
+            </li><!--Sakura-->
+            <li id="gribs-bg">
+                <i class="fa fa-slack" aria-hidden="true"></i>
+            </li><!--Grids-->
+            <li id="KAdots-bg">
+                <i class="iconfont icon-dots"></i>
+            </li><!--Dots-->
+            <li id="totem-bg">
+                <i class="fa fa-superpowers" aria-hidden="true"></i>
+            </li><!--Orange-->
+            <li id="pixiv-bg">
+                <i class="iconfont icon-pixiv"></i>
+            </li><!--Start-->
+            <li id="bing-bg">
+                <i class="iconfont icon-bing"></i>
+            </li><!--Bing-->
+            <li id="dark-bg">
+                <i class="fa fa-moon-o" aria-hidden="true"></i>
+            </li><!--Night-->
+        </ul>
+    </div>
+    <div class="font-family-controls row-container">
+        <button type="button" class="control-btn-serif selected" data-mode="serif" 
+                onclick="mashiro_global.font_control.change_font()">Serif</button>
+        <button type="button" class="control-btn-sans-serif" data-mode="sans-serif" 
+                onclick="mashiro_global.font_control.change_font()">Sans Serif</button>
+    </div>
+</div>
+<canvas id="night-mode-cover"></canvas>
+<?php if (akina_option('sakura_widget')) : ?>
+	<aside id="secondary" class="widget-area" role="complementary" style="left: -400px;">
+    <div class="heading"><?php _e('Widgets') /*小工具*/ ?></div>
+    <div class="sakura_widget">
+	<?php if (function_exists('dynamic_sidebar') && dynamic_sidebar('sakura_widget')) : endif; ?>
+	</div>
+	<div class="show-hide-wrap"><button class="show-hide"><svg xmlns="http://www.w3.org/2000/svg" version="1.1" viewBox="0 0 32 32"><path d="M22 16l-10.105-10.6-1.895 1.987 8.211 8.613-8.211 8.612 1.895 1.988 8.211-8.613z"></path></svg></button></div>
+    </aside>
+<?php endif; ?>
+<?php if (akina_option('aplayer_server') != 'off'): ?>
+    <div id="aplayer-float" style="z-index: 100;"
+	    class="aplayer"
+        data-id="<?php echo akina_option('aplayer_playlistid', ''); ?>"
+        data-server="<?php echo akina_option('aplayer_server'); ?>"
+        data-type="playlist"
+        data-fixed="true"
+        data-theme="orange">
+    </div>
+<?php endif; ?>
+</body>
+</html>
